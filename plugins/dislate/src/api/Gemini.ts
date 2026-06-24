4486a9e77182f231285998f603e8de4c182fb68d
WestonLuca <130226009+WestonLuca@users.noreply.github.com>
2026-06-24 02:19:17 +0100

    Fix GEMINI_API_KEY formatting and update API call

--- plugins/dislate/src/api/Gemini.ts
+++ plugins/dislate/src/api/Gemini.ts
@@ -1,12 +1,11 @@
-const GEMINI_API_KEY = "AQ.Ab8RN6ITpFs6CiKJI6wVyTxP-y_B_PyXbH3QZle9Etf9Ax1MXw"
+const GEMINI_API_KEY = "Ab8RN6ITpFs6CiK JI6wVyTXP-y_B_PyXbH 3QZle9Etf9Ax1MXw"
 
 const translate = async (text: string, source_lang: string = "auto", target_lang: string, original: boolean = false) => {
     try {
         if (original) return { source_lang, text }
 
         const response = await fetch(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${"AQ.Ab8RN6ITpFs6CiKJI6wVyTxP-y_B_PyXbH3QZle9Etf9Ax1MXw"
-                                                                                                          }`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
