ef0ada39a1ef8d82f7b7c38c16c2314cee412618
WestonLuca <130226009+WestonLuca@users.noreply.github.com>
2026-06-24 13:44:10 +0100

    Update GEMINI_API_KEY to remove prefix 'AQ.'

--- plugins/dislate/src/api/Gemini.ts
+++ plugins/dislate/src/api/Gemini.ts
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = "AQ.Ab8RN6LVhOwJFsD68GrrJGztvBgWWu_1L8m-tRREVDQeEr4DFA"
+const GEMINI_API_KEY = "Ab8RN6LVhOwJFsD68GrrJGztvBgWWu_1L8m-tRREVDQeEr4DFA"
 
 const translate = async (
     text: string,
